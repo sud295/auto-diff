4166057f971a1c3e625d828004044ad087c8a85c
sud295 <sonicsid12@gmail.com>
2023-08-01 12:07:46 -0400

    Removed security flaw with eval

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from auto_diff import *
 from flask import Flask, render_template, request
+import re
 
 app = Flask(__name__)
 
@@ -28,20 +29,43 @@ def index():
     
     return render_template('index.html')
 
-def calculate_partials(variables: dict, function: list[str]):
+def calculate_partials(variables: dict, function: str):
+    allowed_names = {"Log", "Sin", "Cos", "Variable", "Constant", "Add", "Subtract", "Multiply", "Divide", "+", "-", "*", "/"}
+    for i in range(97, 123):
+        allowed_names.add(chr(i))
+
     Graph()
 
     for_function = {}
     for variable in variables:
         for_function[variable] = Variable(variables.get(variable),variable)
+    
+    symbols = re.findall(r'\b\w+\b', function)
+
+    # Check if all symbols are allowed to prevent misuse of program
+    for symbol in symbols:
+        if symbol not in allowed_names:
+            return ValueError(f"'{symbol}' is not an allowed symbol.")
         
-    output = eval(function, None, for_function)
+    try:
+        output = eval(function, None, for_function)
+    except:
+        return "That didn't work! Try again."
 
     backward_pass()
 
     partials = get_partials()
 
-    return partials
+    out_str = ""
+    for partial in partials:
+        out_str += str(partial[0])
+        out_str += "="
+        out_str += str(partial[1])
+        out_str += "\n"
+    
+    out_str = out_str[:-1]
+
+    return out_str
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
